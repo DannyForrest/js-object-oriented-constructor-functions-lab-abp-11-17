7f741dd242e114ff114623ab449d54f171cbafb3
Danny Forrest <dannyforrest@gmail.com>
2017-11-14 00:36:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function Scooter(year, color, model) {
 }
 
 new Scooter(2010, "yellow", "crappy");
+
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+new Driver("Bob", 43, 4);
